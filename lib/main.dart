0a99b6d96989d8a8b7eff6080e680c7ecf4de18c
vrushita-sohamtech <vrushita.sohamtech@gmail.com>
2023-04-27 17:13:35 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Photos(),
+      title: 'MY PHOTOS',
+    );
+  }
+}
+
+class Photos extends StatefulWidget {
+  @override
+  State<Photos> createState() => _PhotosState();
+}
+
+class _PhotosState extends State<Photos> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text('MY PHOTOS'),
+        centerTitle: true,
+      ),
+    );
+  }
+}
